6315adf42f70038b2de55d8f51fe76a976c756ad
Sallbertone <sallbertone@gmail.com>
2018-05-21 21:01:59 +0200

    Web scraping for proverbs and meanings and saving data in DB (Hibernate)

--- src/main/java/pl/tatastruga/web/Main.java
+++ src/main/java/pl/tatastruga/web/Main.java
@@ -1,53 +1,138 @@
 package pl.tatastruga.web;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main
 {
 
 	public static void main(String[] args)
 	{
-		Proverb proverb = new Proverb("W marcu jak w garncu", "Przysłowie określające zmienność marcowej pogody");
-		Proverb proverb2 = new Proverb("Czego Jaś się nie nauczy, tego Jan nie będzie umiał!",
-				"Przysłowie to mówi nam, że wiedza i umiejętności, które zdobywamy w czasie dzieciństwa pozostają nam na całe życie. \nDlatego ważne jest aby w dzieciństwie nabrać odpowiednich cech charakteru, które ukształtują nas na resztę naszego życia.");
 
-		 Connection myConn = null;
-		 Statement myStmt = null;
-		 ResultSet myRs = null;
-		 
-		 String dbUrl = "jdbc:mysql://s43.linuxpl.com/creall_proverbs";
-		 String user = "creall_provUser";
-		 String pass = "tzfyJExk";
+		ArrayList<String> proverbsList = new ArrayList<String>();
+		ArrayList<String> meaningsList = new ArrayList<String>();
 
-		 try 
-		 {
-		    myConn =  DriverManager.getConnection(dbUrl, user, pass);
-		 
-		    System.out.println("Połączono z bazą");
-		    
-		    myStmt  =  myConn.createStatement();
-		    
-		    myRs = myStmt.executeQuery("SELECT id, meaning FROM proverbs WHERE proverb LIKE '%bab%'  ");
-		    
-		    while(myRs.next())
-		    {
-		    System.out.println(myRs.getString("id")+". "+myRs.getString("meaning"));
-		     }
-		       
-		 } catch (SQLException ex)
-		 {
-			 ex.printStackTrace();
-		 }
+		Proverb proverb = null;
+
+		Document doc = null;
+
+		// connection to web page for scraping
+		try
+		{
+			doc = Jsoup.connect("https://www.polskatradycja.pl/folklor/przyslowia.html").userAgent("Mozilla/5.0").get();
+			System.out.println("connected");
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+		// web scraping for proverbs and storing in list
+		Elements webProverbs = doc.select("tr.row0");
+
+		for (Element tempWebProverb : webProverbs)
+		{
+			proverbsList.add(tempWebProverb.getElementsByTag("a").first().text());
+		}
+
+		webProverbs = doc.select("tr.row1");
+
+		for (Element tempWebProverb : webProverbs)
+		{
+			proverbsList.add(tempWebProverb.getElementsByTag("a").first().text());
+		}
+
+		// web scraping for meanings and storing in list
+		Elements webMeanings = doc.select("tr.row0");
+
+		for (Element tempWebMeanings : webMeanings)
+		{
+			meaningsList.add(tempWebMeanings.getElementsByTag("p").first().text());
+		}
+
+		webMeanings = doc.select("tr.row1");
+
+		for (Element tempWebMeanings : webMeanings)
+		{
+			meaningsList.add(tempWebMeanings.getElementsByTag("p").first().text());
+		}
+
 		
-		
-		
-		
+		// create Hibernate session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Proverb.class)
+									.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try
+		{
+			//saving proverbs and meanings in DB
+			for (int i = 0; i < proverbsList.size(); i++)
+			{
+				// create a Proverb oject
+				Proverb tempProverb = new Proverb(proverbsList.get(i), meaningsList.get(i));
+				
+				session.beginTransaction();
+				session.save(tempProverb);
+				session.getTransaction().commit();
+				session.close();
+				
+				//restoring session
+				session = factory.getCurrentSession();
+			}
+
+		} 
+		finally
+		{
+			factory.close();
+		}
+
+		//testing DB connection and stored data
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+
+		String dbUrl = "jdbc:mysql://s43.linuxpl.com/creall_proverbs";
+		String user = "creall_provUser";
+		String pass = "tzfyJExk";
+
+		try
+		{
+			myConn = DriverManager.getConnection(dbUrl, user, pass);
+
+			System.out.println("Połączono z bazą");
+
+			myStmt = myConn.createStatement();
+
+			myRs = myStmt.executeQuery("SELECT * FROM proverbs WHERE id=60");
+
+			while (myRs.next())
+			{
+				proverb = new Proverb(myRs.getInt("id"), myRs.getString("proverb"), myRs.getString("meaning"));
+			}
+
+		} catch (SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+
 		Scanner input = new Scanner(System.in);
 
 		char[] proverbChars = proverb.getProverb().toCharArray();
@@ -60,7 +145,8 @@ public class Main
 		for (int i = 0; i < proverbChars.length; i++)
 		{
 			if (proverbChars[i] == ',' || proverbChars[i] == ' ' || proverbChars[i] == '-' || proverbChars[i] == '?'
-					|| proverbChars[i] == '!' || proverbChars[i] == '.')
+					|| proverbChars[i] == '!' || proverbChars[i] == '.' || proverbChars[i] == ':'
+					|| proverbChars[i] == ';')
 			{
 				hiddenProverbChars[i] = proverbChars[i];
 			} else
@@ -116,13 +202,13 @@ public class Main
 				System.out.println(missedShots);
 				System.out.println(missedShotsCounter + " (" + missedShotsCounter.length() + "/10)");
 				System.out.println();
-			} 
-			
+			}
+
 			if (missedShotsCounter.length() == 10)
 			{
 
-				System.out.println("Pudło po raz 10! Nie udało Ci się odgadnąć hasła. \n\nPrzysłowie brzmi: "
-						+ "\"" + proverb.getProverb() + "\"");
+				System.out.println("Pudło po raz 10! Nie udało Ci się odgadnąć hasła. \n\nPrzysłowie brzmi: " + "\""
+						+ proverb.getProverb() + "\"");
 				break;
 			}
 
